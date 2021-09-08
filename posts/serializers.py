7d9e9677e5b694df7e642625b54d93bd0d029a73
DustySvensson <nessaswanson@hotmail.com>
2021-09-08 21:53:35 +0100

    created simple serializers for images and tags on posts so can see in Insomnia request

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Image, Tag
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('id', 'image_link')
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'tag')
 
 class PostSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
     class Meta:
         model=Post
         fields='__all__'
+
